ee226ea9d335c0b17d4b7d7d8090ae2b01aea8c2
Dipali <dipalimanu15@gmail.com>
2024-11-19 19:19:58 +0530

    added/modified/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,6 +4,11 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("LP");
+		int a=10;
+		int b=20;
+		System.out.println(a);
+		System.out.println(b);
+
 
 	}
 
--- src/tests/LoginPageTest.java
+++ src/tests/LoginPageTest.java
@@ -2,4 +2,8 @@ package tests;
 
 public class LoginPageTest {
 
+	public void test1()
+	{
+		System.out.println("test1-LP");
+	}
 }
